b4a4459923cf96f1cce769601af898e2cf11613d
Hbixler <hbixler@unomaha.edu>
2026-04-01 19:42:39 -0700

    objects

---
+++ fridge/cookies.js
@@ -0,0 +1,19 @@
+let cookies = [
+    {
+        name: 'Basic Cookie',
+        description: "He's a little basic but you pretend to love him because it's inextricably tied to his sense of self-worth",
+        ingredients: ['cookie']
+    },
+    {
+        name: 'Chocolate Chip Cookie',
+        description: "Basic Cookie's cooler older cousin",
+        ingredients: ['cookie', 'chocolate chip']
+    },
+    {
+        name: 'Monster Cookie',
+        description: "AAAAH IT'S A MONSTER QUICK EAT IT!!!!!",
+        ingredients: ['cookie', 'peanut butter', 'oatmeal', 'chocolate chip']
+    }
+]
+
+export default cookies
---
+++ fridge/ingredients.js
@@ -0,0 +1,30 @@
+let ingredients = [
+    {
+        name: 'Cookie',
+        id: 'cookie',
+        description: 'galleta?',
+        cost: 0
+    },
+    {
+        name: 'Peanut Butter',
+        id: 'peanut butter',
+        description: 'mmm nom nom mmmmmmm',
+        cost: 5
+    },
+    {
+        name: 'Oatmeal',
+        id: 'oatmeal',
+        description: 'good for horses and also for humans!',
+        cost: 7
+    },
+    {
+        name: 'Chocolate Chip',
+        id: 'chocolate chip',
+        description: 'hephaestus plays poker with these in his spare time',
+        cost: 3
+    }
+]
+
+export default ingredients
+
+// ps kai will be mad that these aren't in order hehe
