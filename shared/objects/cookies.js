9f06858e58a24bd3babf3596cf9ed44644f4f0a3
Hbixler <hbixler@unomaha.edu>
2026-06-10 00:21:44 -0700

    inventory

--- shared/objects/cookies.js
+++ shared/objects/cookies.js
@@ -1,16 +1,19 @@
 let cookies = [
     {
         name: "Basic Cookie",
+        id: 'cookie-basic',
         description: "He's a little basic but you pretend to love him because it's inextricably tied to his sense of self-worth",
         ingredients: ['ingredient-cookie']
     },
     {
         name: "Chocolate Chip Cookie",
+        id: 'cookie-chocolate-chip',
         description: "Basic Cookie's cooler older cousin",
         ingredients: ['ingredient-cookie', 'ingredient-chocolate-chip']
     },
     {
         name: "Monster Cookie",
+        id: 'cookie-monster',
         description: "AAAAH IT'S A MONSTER QUICK EAT IT!!!!!",
         ingredients: ['ingredient-cookie', 'ingredient-peanut-butter', 'ingredient-oatmeal', 'ingredient-chocolate-chip']
     }
